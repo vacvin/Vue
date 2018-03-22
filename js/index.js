169528f38df588730c4f4d68daece428d7002953
vince.su <vacvin@gmail.com>
2018-03-22 11:01:21 +0800

    practice: v-bind:title

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ init = function () {
 	var app = new Vue({
 	  el: '#app',
 	  data: {
-	    message: 'Hello Vue!'
+	    message: 'You loaded this page on ' + new Date().toLocaleString()
 	  }
-	});
+	})
 }
