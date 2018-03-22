e855888743ca96b5397eec17483132cdfbffe0f0
vince.su <vacvin@gmail.com>
2018-03-22 13:36:12 +0800

    Computed Properties practice

--- js/index.js
+++ js/index.js
@@ -78,4 +78,26 @@ init = function () {
 			]
 		}
 	})
+
+	var app8 = new Vue({
+		el: '#app8',
+		data: {
+			firstName: 'Foo',
+			lastName: 'Bar'
+		},
+		computed: {
+			fullName: {
+				get: function () {
+					return this.firstName + ' ' + this.lastName;
+			    },
+				set: function (newValue) {
+					var names = newValue.split(' ');
+					this.firstName = names[0];
+					this.lastName = names[names.length - 1];
+				}
+			}
+		}
+	})
+
+	app8.fullName = 'John Doe';
 }
