01da3f088a7ba6d4d687687088ed878a5e013587
vince.su <vacvin@gmail.com>
2018-03-22 11:55:13 +0800

    Practice: Component

--- js/index.js
+++ js/index.js
@@ -58,4 +58,24 @@ init = function () {
 	    message: 'Hello Vue!'
 	  }
 	})
+
+	// Define a new component called todo-item
+	Vue.component('todo-item', {
+		// The todo-item component now accepts a
+		// "prop", which is like a custom attribute.
+		// This prop is called todo.
+		props: ['todo'],
+		template: '<li>{{ todo.text }}</li>'
+	})
+
+	var app7 = new Vue({
+		el: '#app7',
+		data: {
+			groceryList: [
+				{ id: 0, text: 'Vegetables' },
+				{ id: 1, text: 'Cheese' },
+				{ id: 2, text: 'Whatever else humans are supposed to eat' }
+			]
+		}
+	})
 }
