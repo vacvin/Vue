8ae1a07ebd95dc26ea45a66fdb4c90233cbd7e1b
vince.su <vacvin@gmail.com>
2018-03-22 11:42:13 +0800

    practice: button click to reverse message

--- js/index.js
+++ js/index.js
@@ -39,4 +39,16 @@ init = function () {
 	    ]
 	  }
 	})
+
+	var app5 = new Vue({
+	  el: '#app5',
+	  data: {
+	    message: 'Hello Vue.js!'
+	  },
+	  methods: {
+	    reverseMessage: function () {
+	      this.message = this.message.split('').reverse().join('')
+	    }
+	  }
+	})
 }
