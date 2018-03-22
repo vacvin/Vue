f5a13970c3e9914833972fb5fe6f73c93b69a70e
vince.su <vacvin@gmail.com>
2018-03-22 11:28:43 +0800

    practice: v-if, v-on:mouseover

--- js/index.js
+++ js/index.js
@@ -3,10 +3,29 @@ window.onload = function () {
 }
 
 init = function () {
-	var app = new Vue({
-	  el: '#app',
-	  data: {
-	    message: 'You loaded this page on ' + new Date().toLocaleString()
-	  }
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Hello Vue!'
+      }
+    });
+
+    var app2 = new Vue({
+      el: '#app2',
+      data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+      }
+    });
+
+	var app3 = new Vue({
+		el: '#app3',
+		data: {
+			seen: true
+		},
+	    methods: {
+	        hideMessage: function(){
+	            this.seen = false;
+	        }
+	    }
 	})
 }
